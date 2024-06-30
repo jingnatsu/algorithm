74db2e41b395f0e617e4d45a2e5fe92302a64de2
simonnguyen <nnguyen92.tdtit@gmail.com>
2024-07-01 00:01:43 +0700

    add new 2 problems

---
+++ AddTwoNumbers.js
@@ -0,0 +1,16 @@
+class NodeList {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function addTwoNumbers(l1, l2) {
+    let dummy = new NodeList();
+    let current = dummy;
+    let carry = 0;
+
+    while(l1 !== null || l2!== null || carry !==0) {
+        
+    }
+}
---
+++ TwoSum.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+    const numToIndex = {};
+
+    for(let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num;
+        if(numToIndex.hasOwnProperty(complement)) {
+            return [numToIndex[complement], i];
+        }
+
+        numToIndex[complement] = i;
+
+    }
+    return [];
+}
