65813e964766c43a08047d1f3923ce87bc9f2ad8
jingnatsu <65613298+jingnatsu@users.noreply.github.com>
2024-07-05 13:58:44 +0700

    Update BestTimeToBuyAndSellStock.js

--- BestTimeToBuyAndSellStock.js
+++ BestTimeToBuyAndSellStock.js
@@ -25,12 +25,14 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 var maxProfit = function(prices) {
     let buyPrice = prices[0];
     let profit = 0;
-    for(let i = 1; i<prices.length; i++) {
+    for(let i = 1; i < prices.length; i++) {
         if (buyPrice > prices[i]) {
-            buyPrice = prices[i]
+            buyPrice = prices[i];
         }
 
         profit = Math.max(profit, prices[i] - buyPrice);
     }
     return profit;
 };
+
+console.log(maxProfit([7,6,4,3,1]));
